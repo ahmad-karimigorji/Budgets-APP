f84b56a98349ce70a76388c8e8e914542bf392ee
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2023-03-05 13:14:51 +0330

    add logic forms

--- src/component/ExpenseForm.js
+++ src/component/ExpenseForm.js
@@ -1,21 +1,83 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./ExpenseForm.module.scss";
+import { addExpense } from "../redux/expense/expenseActions";
 
-const ExpenseForm = ({setIsExpenseForm}) => {
+const ExpenseForm = ({ setIsExpenseForm }) => {
+  const dispatch = useDispatch();
+  const { budgets } = useSelector((state) => state.budget);
+  const [formValue, setFormValue] = useState({
+    category: "",
+    Description: "",
+    cost: "",
+  });
+  const [error, setError] = useState(false);
+  const changeHandler = ({ target }) => {
+    setError(false);
+
+    const { name, value } = target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!formValue.category.trim() || parseInt(formValue.cost.charAt()) < 1) {
+      setError(true);
+      return;
+    }
+    // console.log(formValue);
+    dispatch(addExpense(formValue));
+    setFormValue({ category: "", Description: "", cost: "" });
+  };
+  if(!budgets.length){
+    return <h3 className={styles.warning}>Enter new budget !!</h3>
+  }
   return (
-    <form className={styles.expenseForm}>
+    <form className={styles.expenseForm} onSubmit={submitHandler}>
       <div>
         <label>Expense Category</label>
-        <select>
+        <select
+          name="category"
+          value={formValue.category}
+          onChange={changeHandler}
+        >
           <option value="">Select category</option>
+          {budgets.map((item) => (
+            <option value={item.budget}>{item.budget}</option>
+          ))}
         </select>
       </div>
       <div>
-        <label>Budget Value</label>
-        <input type="number" min={1} />
+        <label>Description</label>
+        <input
+          type="text"
+          value={formValue.Description}
+          name="Description"
+          onChange={changeHandler}
+        />
       </div>
+      <div>
+        <label>Expense Value</label>
+        <input
+          type="number"
+          min={1}
+          value={formValue.cost}
+          name="cost"
+          onChange={changeHandler}
+        />
+      </div>
+      {error && <p className={styles.error}>Enter Valid Data !!</p>}
       <div className={styles.formBtnBox}>
-        <button className={styles.addBtn}>Add Budget</button>
-        <button className={styles.cancelBtn} onClick={() => setIsExpenseForm(false)}>Cancel</button>
+        <button type="submit" className={styles.addBtn}>
+          Add Expense
+        </button>
+        <button
+          type="button"
+          className={styles.cancelBtn}
+          onClick={() => setIsExpenseForm(false)}
+        >
+          Cancel
+        </button>
       </div>
     </form>
   );
--- src/component/OverViewComponent.js
+++ src/component/OverViewComponent.js
@@ -1,50 +1,96 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBudget } from "../redux/budget/budgetActions";
 import ExpenseForm from "./ExpenseForm";
 import styles from "./OverViewComponent.module.scss";
 
 const OverViewComponent = () => {
-    const [isBudgetForm, setIsBudgetForm] =useState(false)
-    const [isExpenseForm, setIsExpenseForm] =useState(false)
+  const [isBudgetForm, setIsBudgetForm] = useState(false);
+  const [isExpenseForm, setIsExpenseForm] = useState(false);
 
-    const budgetFormHandler = () => {
-        setIsBudgetForm(true)
-        setIsExpenseForm(false)
-    }
-    const expenseFormHandler = () => {
-        setIsExpenseForm(true)
-        setIsBudgetForm(false)
-    }
+  const budgetFormHandler = () => {
+    setIsBudgetForm(!isBudgetForm);
+    setIsExpenseForm(false);
+  };
+  const expenseFormHandler = () => {
+    setIsExpenseForm(!isExpenseForm);
+    setIsBudgetForm(false);
+  };
   return (
     <div className={styles.overView}>
       <div className={styles.overViewHeader}>
         <h2>Budgets</h2>
         <div>
-          <button className={styles.budgetBtn} onClick={budgetFormHandler}>Add Budget</button>
-          <button className={styles.expenseBtn} onClick={expenseFormHandler}>Add Expense</button>
+          <button className={styles.budgetBtn} onClick={budgetFormHandler}>
+            Add Budget
+          </button>
+          <button className={styles.expenseBtn} onClick={expenseFormHandler}>
+            Add Expense
+          </button>
         </div>
       </div>
-      {isBudgetForm && <BudgetForm setIsBudgetForm={setIsBudgetForm}/>}
-      {isExpenseForm && <ExpenseForm setIsExpenseForm={setIsExpenseForm}/>}
+      {isBudgetForm && <BudgetForm setIsBudgetForm={setIsBudgetForm} />}
+      {isExpenseForm && <ExpenseForm setIsExpenseForm={setIsExpenseForm} />}
     </div>
   );
 };
 
 export default OverViewComponent;
 
-const BudgetForm = ({setIsBudgetForm}) => {
+const BudgetForm = ({ setIsBudgetForm }) => {
+  const [formValue, setFormValue] = useState({ budget: "", amount: "" });
+  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+  const changeHandler = ({ target }) => {
+    setError(false);
+
+    const { name, value } = target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!formValue.budget.trim() || parseInt(formValue.amount.charAt()) < 1) {
+      setError(true);
+      return;
+    }
+    // console.log(formValue);
+    dispatch(addBudget(formValue));
+    setFormValue({ budget: "", amount: "" });
+  };
   return (
-    <form className={styles.budgetForm}>
+    <form className={styles.budgetForm} onSubmit={submitHandler}>
       <div>
         <label>Budget Name</label>
-        <input type="text" />
+        <input
+          type="text"
+          name="budget"
+          value={formValue.budget}
+          onChange={changeHandler}
+        />
       </div>
       <div>
         <label>Budget Value</label>
-        <input type="number" min={1} />
+        <input
+          type="number"
+          min={1}
+          name="amount"
+          value={formValue.amount}
+          onChange={changeHandler}
+        />
       </div>
+      {error && <p className={styles.error}>Enter Valid Data !!</p>}
       <div className={styles.formBtnBox}>
-        <button className={styles.addBtn}>Add Budget</button>
-        <button className={styles.cancelBtn} onClick={() => setIsBudgetForm(false)}>Cancel</button>
+        <button type="submit" className={styles.addBtn}>
+          Add Budget
+        </button>
+        <button
+          type="button"
+          className={styles.cancelBtn}
+          onClick={() => setIsBudgetForm(false)}
+        >
+          Cancel
+        </button>
       </div>
     </form>
   );
