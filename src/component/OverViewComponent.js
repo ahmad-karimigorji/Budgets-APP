7fa7c47d50a0b34c7c4dea876ac4002cf840a523
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2023-03-06 17:56:54 +0330

    refactor forms

--- src/component/OverViewComponent.js
+++ src/component/OverViewComponent.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addBudget } from "../redux/budget/budgetActions";
-import ExpenseForm from "./ExpenseForm";
+import { addExpense } from "../redux/expense/expenseActions";
 import styles from "./OverViewComponent.module.scss";
 
 const OverViewComponent = ({ isExpenseForm, setIsExpenseForm }) => {
@@ -37,6 +37,7 @@ const OverViewComponent = ({ isExpenseForm, setIsExpenseForm }) => {
 export default OverViewComponent;
 
 const BudgetForm = ({ setIsBudgetForm }) => {
+
   const [formValue, setFormValue] = useState({ budgetName: "", amount: "" });
   const [error, setError] = useState(false);
   const dispatch = useDispatch();
@@ -68,7 +69,7 @@ const BudgetForm = ({ setIsBudgetForm }) => {
     setIsBudgetForm(false);
   };
   return (
-    <form className={styles.budgetForm} onSubmit={submitHandler}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div>
         <label>Budget Name</label>
         <input
@@ -97,6 +98,98 @@ const BudgetForm = ({ setIsBudgetForm }) => {
           type="button"
           className={styles.cancelBtn}
           onClick={() => setIsBudgetForm(false)}
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
+  );
+};
+
+
+const ExpenseForm = ({ setIsExpenseForm }) => {
+  
+  const dispatch = useDispatch();
+  const { budgets } = useSelector((state) => state.budget);
+  const [formValue, setFormValue] = useState({
+    category: "",
+    description: "",
+    cost: "",
+  });
+  const [error, setError] = useState(false);
+  const changeHandler = ({ target }) => {
+    setError(false);
+
+    const { name, value } = target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!formValue.category.trim() || !formValue.category.trim() || parseInt(formValue.cost.charAt()) < 1) {
+      setError(true);
+      return;
+    }
+    // console.log(formValue);
+    const data = {
+      ...formValue,
+      cost: parseInt(formValue.cost),
+      category: parseInt(formValue.category),
+      id: new Date().getTime(),
+      creatAt: new Date().toISOString(),
+    };
+    dispatch(addExpense(data));
+    setFormValue({ category: "", description: "", cost: "" });
+    setIsExpenseForm(false)
+  };
+  if (!budgets.length) {
+    return <h3 className={styles.warning}>Enter new budget !!</h3>;
+  }
+  return (
+    <form className={styles.form} onSubmit={submitHandler}>
+      <div>
+        <label>Expense Category</label>
+        <select
+          name="category"
+          value={formValue.category}
+          onChange={changeHandler}
+        >
+          <option value="">Select category</option>
+          {budgets.map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.budgetName}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label>Description</label>
+        <input
+          type="text"
+          value={formValue.description}
+          name="description"
+          onChange={changeHandler}
+        />
+      </div>
+      <div>
+        <label>Expense Value</label>
+        <input
+          type="number"
+          min={1}
+          value={formValue.cost}
+          name="cost"
+          onChange={changeHandler}
+        />
+      </div>
+      {error && <p className={styles.error}>Enter Valid Data !!</p>}
+      <div className={styles.formBtnBox}>
+        <button type="submit" className={styles.addBtn}>
+          Add Expense
+        </button>
+        <button
+          type="button"
+          className={styles.cancelBtn}
+          onClick={() => setIsExpenseForm(false)}
         >
           Cancel
         </button>
